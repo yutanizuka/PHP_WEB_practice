a06027aa462f822d1eb71b08a4b0e48de1200404
nizutaro <niduka81@gmail.com>
2023-01-17 01:53:28 +0900

    first commit

---
+++ work/web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$today = date('Y-m-d H:i:s');
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <p>Hello, PHP!</p>
+  <p>Today: <?= $today  ?></p>
+
+</body>
+</html>
