e88768a3f106c451923123545ed39a5eac11ce03
nizutaro <niduka81@gmail.com>
2023-01-24 08:03:29 +0900

    #34 CSRF

--- work/app/functions.php
+++ work/app/functions.php
@@ -5,3 +5,22 @@ function h($str)
 { 
   return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
 }
+
+function createToken()
+{
+  if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken()
+{
+  if (
+    empty($_SESSION['token']) ||
+    $_SESSION['token'] !== filter_input(INPUT_POST , 'token')
+  ) {
+    exit('Invalid post request');
+  }
+}
+
+session_start();
--- work/web/index.php
+++ work/web/index.php
@@ -2,9 +2,12 @@
 
 require('../app/functions.php');
 
+createToken();
+
 define('FILENAME', '../app/messages.txt');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
   $message = trim(filter_input(INPUT_POST, 'message'));
   $message = $message !== '' ? $message : '...';
   
@@ -34,6 +37,7 @@ include('../app/_parts/_header.php');
 <form action="" method="post">
   <input type="text" name="message">
   <button>Post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 
